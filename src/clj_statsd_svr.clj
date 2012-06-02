9a9ac98dfc4a12eb3a9717d3b8cb5719027d579e
scarytom <t.denley@cantab.net>
2012-06-02 15:03:13 +0100

    refactor to producer/consumer

--- src/clj_statsd_svr.clj
+++ src/clj_statsd_svr.clj
@@ -2,13 +2,22 @@
   "statsd protocol server"
   (:import [java.net DatagramPacket DatagramSocket InetAddress]))
 
-(def work (java.util.concurrent.LinkedBlockingQueue.))
-
 (def port 8125)
-(def socket (DatagramSocket. port))
 
-(defn receive
+(defn receive [socket queue]
   (let [size 1024
         packet (DatagramPacket. (byte-array size) size)]
-  (.receive socket packet)
-  (.put work (String. (.getData packet) 0 (.getLength packet) "UTF-8"))))
+    (.receive socket packet)
+    (.put queue (String. (.getData packet) 0 (.getLength packet) "UTF-8"))))
+
+(defn listener [port-no queue]
+  (let [socket (DatagramSocket. port-no)]
+    (.start (Thread. #((receive socket queue) (recur))))))
+
+(defn consumer [queue]
+  (println (.take queue))
+  (recur queue))
+
+(let [work (java.util.concurrent.LinkedBlockingQueue.)]
+  (listener port work)
+  (consumer work))
