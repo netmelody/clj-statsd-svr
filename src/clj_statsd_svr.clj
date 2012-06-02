5f8c0faa8d4c4f50e20d3b2cfd00a7c0cfe9928f
scarytom <t.denley@cantab.net>
2012-06-02 11:08:28 +0100

    read udp packets

--- src/clj_statsd_svr.clj
+++ src/clj_statsd_svr.clj
@@ -2,3 +2,12 @@
   "statsd protocol server"
   (:import [java.net DatagramPacket DatagramSocket InetAddress]))
 
+(def port 8125)
+(def socket (DatagramSocket. port))
+  
+(defn receive
+  (let [size 32
+        data (byte-array size)
+        packet (DatagramPacket. data size)]
+  (.receive socket packet)
+  (new String (.getData receive-packet) 0 (.getLength receive-packet))))
