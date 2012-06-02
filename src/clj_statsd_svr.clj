fdf6013190a60d3dd22b1fbfc521b421b1ea29af
scarytom <t.denley@cantab.net>
2012-06-02 19:09:01 +0100

    define handlers in thread pool

--- src/clj_statsd_svr.clj
+++ src/clj_statsd_svr.clj
@@ -1,23 +1,29 @@
 (ns clj-statsd-svr
   "statsd protocol server"
-  (:import [java.net DatagramPacket DatagramSocket InetAddress]))
+  (:import [java.net DatagramPacket DatagramSocket]))
 
 (def port 8125)
 
-(defn receive [socket queue]
+(defn receive [socket]
   (let [size 1024
         packet (DatagramPacket. (byte-array size) size)]
     (.receive socket packet)
-    (.put queue (String. (.getData packet) 0 (.getLength packet) "UTF-8"))))
+    (String. (.getData packet) 0 (.getLength packet) "UTF-8")))
 
-(defn listener [port-no queue]
+(defn new-listener [port-no queue]
   (let [socket (DatagramSocket. port-no)]
-    (.start (Thread. #((receive socket queue) (recur))))))
+    (.start (Thread. #(while true (.put queue (receive socket)))))
+    socket))
 
-(defn consumer [queue]
-  (println (.take queue))
-  (recur queue))
+(defn handle [data]
+  (print data))
 
-(let [work (java.util.concurrent.LinkedBlockingQueue.)]
-  (listener port work)
-  (consumer work))
+(defn new-worker [queue]
+  #(while true (handle (.take queue))))
+
+(defn start []
+  (let [worker-count 2
+        work-queue (java.util.concurrent.LinkedBlockingQueue.)
+        executor (java.util.concurrent.Executors/newFixedThreadPool worker-count)]
+    (new-listener port work-queue)
+    (doall (for [_ (range worker-count)] (.submit executor (new-worker work-queue))))))
