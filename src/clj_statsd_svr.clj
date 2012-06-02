94ab5b5ff246566ae6fe8900a1318f192bdc9563
scarytom <t.denley@cantab.net>
2012-06-02 14:15:29 +0100

    receive datagram packet and put data onto the work queue

--- src/clj_statsd_svr.clj
+++ src/clj_statsd_svr.clj
@@ -2,12 +2,14 @@
   "statsd protocol server"
   (:import [java.net DatagramPacket DatagramSocket InetAddress]))
 
+(def work (java.util.concurrent.LinkedBlockingQueue.))
+
 (def port 8125)
 (def socket (DatagramSocket. port))
-  
+
 (defn receive
-  (let [size 32
+  (let [size 1024
         data (byte-array size)
         packet (DatagramPacket. data size)]
   (.receive socket packet)
-  (new String (.getData receive-packet) 0 (.getLength receive-packet))))
+  (.put work (String. (.getData packet) 0 (.getLength packet) "UTF-8"))))
