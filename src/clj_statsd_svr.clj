194488aab550b56cdee48139daf20c53d90283d7
scarytom <t.denley@cantab.net>
2012-06-02 22:10:28 +0100

    start decoding incoming data

--- src/clj_statsd_svr.clj
+++ src/clj_statsd_svr.clj
@@ -10,20 +10,25 @@
     (.receive socket packet)
     (String. (.getData packet) 0 (.getLength packet) "UTF-8")))
 
-(defn new-listener [port-no queue]
+(defn start-receiver [port-no queue]
   (let [socket (DatagramSocket. port-no)]
     (.start (Thread. #(while true (.put queue (receive socket)))))
     socket))
 
+(defn decode [data]
+  (if-let [[_ bucket value type sample-rate] (re-matches #"(.+):(\d+)\|(c|ms|g)(?:(?<=c)\|@(\d+(?:\.\d+)?))?" data)]
+    {:bucket bucket :value value :type type :sample-rate sample-rate}
+    (println "bad data:" data)))
+
 (defn handle [data]
-  (print data))
+  (process ()))
 
 (defn new-worker [queue]
-  #(while true (handle (.take queue))))
+  #(while true (when-let [record (decode (.take queue))] (handle record))))
 
 (defn start []
   (let [worker-count 2
         work-queue (java.util.concurrent.LinkedBlockingQueue.)
         executor (java.util.concurrent.Executors/newFixedThreadPool worker-count)]
-    (new-listener port work-queue)
+    (start-receiver port work-queue)
     (doall (for [_ (range worker-count)] (.submit executor (new-worker work-queue))))))
