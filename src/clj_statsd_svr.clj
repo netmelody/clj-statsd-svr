a53d47b311e91d3963ab04f73da1cc8989a405c7
scarytom <t.denley@cantab.net>
2012-06-03 13:00:34 +0100

    use an agent for the storage

--- src/clj_statsd_svr.clj
+++ src/clj_statsd_svr.clj
@@ -4,6 +4,19 @@
   (:import [java.net DatagramPacket DatagramSocket]))
 
 (def port 8125)
+(def statistics (agent { :counters {} :timers {} :gauges {} }))
+
+(defn update-stat [stats stat bucket f]
+  (assoc stats stat (assoc (stats stat) bucket (f ((stats stat) bucket)))))
+
+(defn update-stat-val [stats stat bucket value]
+  (let [f ({:counters #(+ value (or % 0))
+            :timers #()
+            :gauges #(value)} stat)]
+    (update-stat stats stat bucket f)))
+
+(defn inc-counter [stats bucket delta]
+  (update-stat stats :counters bucket #(+ delta (or % 0))))
 
 (defn receive [socket]
   (let [size 1024
@@ -20,12 +33,15 @@
 
 (defn decode [data]
   (if-let [[_ bucket value type sample-rate] (re-matches #"(.+):(\d+)\|(c|ms|g)(?:(?<=c)\|@(\d+(?:\.\d+)?))?" data)]
-    (let [nicebucket (re-replace (re-replace (re-replace bucket #"\s+" "_") #"/" "-") #"[^a-zA-Z_\-0-9\.]" "")]
-      {:bucket nicebucket :value value :type type :sample-rate sample-rate})
+    (let [nicebucket (re-replace (re-replace (re-replace bucket #"\s+" "_") #"/" "-") #"[^a-zA-Z_\-0-9\.]" "")
+          nicevalue (/ (Double/parseDouble value) (Double/parseDouble (or sample-rate "1")))
+          nicetype ({"c" :counters "ms" :timers "g" :gauges} type)]
+      {:bucket nicebucket :value nicevalue :type nicetype })
     (println "bad data:" data)))
 
 (defn handle [record]
-  (println (record :bucket)))
+  (println (record :value))
+  (send-off statistics update-stat-val (record :type) (record :bucket) (record :value)))
 
 (defn new-worker [queue]
   #(while true (when-let [record (decode (.take queue))] (handle record))))
