2e2db3f16c991141674f1cbeae1bd7094970da2c
scarytom <t.denley@cantab.net>
2012-06-05 23:12:04 +0100

    grahpite back end work

--- src/backends/graphite.clj
+++ src/backends/graphite.clj
@@ -3,3 +3,9 @@
 (defn publish [{timestamp :timestamp flushInterval :flush-interval counters :counters timers :timers gauges :gauges}]
   (let [epoch (/ timestamp 1000)]
     (println "graphite epoch " epoch)))
+
+(defn graphite [host port]
+  (let [socket (java.net.Socket. host port)
+        writer (java.io.BufferedWriter. (java.io.OutputStreamWriter. (.getOutputStream socket)))]
+    (println "boo")
+    (.close socket)))
