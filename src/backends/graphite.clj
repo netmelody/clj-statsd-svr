4a8502a1bd0169cb2aa79994e5e451019d8ef427
Tom Denley <t.denley@cantab.net>
2012-06-14 21:31:25 +0100

    implement the graphite backend

--- src/backends/graphite.clj
+++ src/backends/graphite.clj
@@ -3,12 +3,27 @@
 (defn status []
   "graphite.status: OK")
 
-(defn publish [{timestamp :timestamp counters :counters timers :timers gauges :gauges} config]
-  (let [epoch (/ timestamp 1000)]
-    (println "graphite epoch " epoch)))
+(defn counter-to-str [[name count]]
+  (str name " " count))
 
-(defn graphite [host port]
+(defn timer-to-str [[name timings]]
+  (str name " " (first timings)))
+
+(defn gauge-to-str [[name value]]
+  (str name " " value))
+
+(defn publish [{timestamp :timestamp counters :counters timers :timers gauges :gauges} config]
+  (let [epoch (/ timestamp 1000)
+        datapoints (concat (map counter-to-str counters)
+                           (map timer-to-str timers)
+                           (map gauge-to-str gauges))]
+    (graphite "localhost" 8003 "stats" epoch datapoints)))
+
+(defn to-graphite-str [prefix datapoint epoch]
+  (str prefix "." datapoint " " epoch "\n"))
+
+(defn graphite [host port prefix epoch datapoints]
   (let [socket (java.net.Socket. host port)
         writer (java.io.BufferedWriter. (java.io.OutputStreamWriter. (.getOutputStream socket)))]
-    (println "boo")
-    (.close socket)))
+    (doseq [datapoint datapoints] (.append writer (to-graphite-str prefix datapoint epoch)))
+    (.close writer)))
