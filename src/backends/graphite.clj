ced8e0ee3435b4d4965082228193c2804af592b9
scarytom <t.denley@cantab.net>
2012-06-19 23:19:11 +0100

    ignore empty timers

--- src/backends/graphite.clj
+++ src/backends/graphite.clj
@@ -6,11 +6,13 @@
 (defn counter-to-str [[name count]]
   (str name " " count))
 
-(defn timer-to-str [[name timings]]
-  (str "timers." name " " (first timings)))
-
 (defn gauge-to-str [[name value]]
   (str "gauges." name " " value))
+
+(defn timer-to-str [[name timings]]
+  (if (empty? timings)
+    []
+    (str "timers." name " " (first timings))))
 
 (defn to-graphite-str [prefix datapoint epoch]
   (str prefix "." datapoint " " epoch "\n"))
@@ -23,7 +25,7 @@
 
 (defn publish [{timestamp :timestamp counters :counters timers :timers gauges :gauges} config]
   (let [epoch (unchecked-divide-int timestamp 1000)
-        datapoints (concat (map counter-to-str counters)
-                           (map timer-to-str timers)
-                           (map gauge-to-str gauges))]
+        datapoints (flatten (concat (map counter-to-str counters)
+                                    (map timer-to-str timers)
+                                    (map gauge-to-str gauges)))]
     (graphite "localhost" 8003 "stats" epoch datapoints)))
